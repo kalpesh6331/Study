ed4ddeb4572bf2383f986bea76d2387e22e492c2
Kalpesh <kalpesh6331@gmail.com>
2019-01-11 13:59:48 +0000

    Added dynamic search in myhaproxy recipe

--- Chef/cookbooks/myhaproxy/recipes/default.rb
+++ Chef/cookbooks/myhaproxy/recipes/default.rb
@@ -12,11 +12,24 @@ haproxy_frontend 'http-in' do
   default_backend 'servers'
 end
 
+#haproxy_backend 'servers' do
+#  server [
+#	  'web1 3.82.93.50:80 maxconn 32',
+#	  'web2 54.196.60.43:80 maxconn 32'
+#  ]
+#end
+
+all_web_nodes = search('node', 'role:web')
+
+servers = []
+
+all_web_nodes.each do |web_node|
+  server = "#{web_node['hostname']} #{web_node['ipaddress']}:80 maxconn 32"
+  servers.push(server)
+end
+
 haproxy_backend 'servers' do
-  server [
-	  'web1 3.82.93.50:80 maxconn 32',
-	  'web2 54.196.60.43:80 maxconn 32'
-  ]
+  server servers
 end
 
 haproxy_service 'haproxy' do
